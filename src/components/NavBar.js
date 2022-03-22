3b6755a0f03fa6dcae3e574166a8438e07d4f34b
NicoCantatore <mroyero@DIAGNOSTICOMAIPU.COM.AR>
2022-03-22 16:55:56 -0300

    v2

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import './Navbar.css'
+
+const NavBar = () => {
+    return (
+        <nav>
+            
+            <ul class="menu">
+                <li><a class="button" href="">libros</a></li>
+                <li><a class="button" href="">about</a></li>
+                <li><a class="button" href="">contacto</a></li>
+            </ul>
+
+        </nav>
+    )
+}
+
+export default NavBar;
